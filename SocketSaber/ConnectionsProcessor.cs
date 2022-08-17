dd76e460485fb8bff8bb36088ed1c605d7744c08
Егор Брон <71507444+EgorBron@users.noreply.github.com>
2022-08-17 10:57:01 +0300

    Add project files.

---
+++ SocketSaber/ConnectionsProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketSaber {
+    public class SockSConnectionsProcessor : IDisposable {
+        TcpListener socket;
+        List<TcpClient> tcpClients = new List<TcpClient>();
+        public SockSConnectionsProcessor(TcpListener sock) {
+            socket = sock;
+        }
+        public void StartAccepting() {
+            new Thread(() => { while (true) { var client = socket.AcceptTcpClient(); Plugin.Log.Info("New connection"); tcpClients.Add(client); }}).Start();
+        }
+        public void SendToAll(string data) {
+            foreach (var client in tcpClients) {
+                var stream = client.GetStream();
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        public void Dispose() {
+            foreach (var client in tcpClients) client.Close();
+            tcpClients.Clear();
+            socket.Stop();
+        }
+    }
+}
---
+++ SocketSaber/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Reflection;
+using IPALogger = IPA.Logging.Logger;
+
+namespace SocketSaber {
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin {
+        internal static Plugin Instance { get; private set; }
+
+        internal static IPALogger Log { get; private set; }
+
+        public static SockSConnectionsProcessor ConnProc { get; private set; }
+
+        private static readonly Harmony harmony = new Harmony("net.blusutils.socksaber");
+
+        [Init]
+        public Plugin(IPALogger logger) {
+            Instance = this;
+            Log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart() {
+            Log.Notice("Preparing SocketSaber...");
+            var sw = Stopwatch.StartNew();
+            string ip = "127.0.0.1";
+            int port = 9999;
+            var socket = new TcpListener(System.Net.IPAddress.Parse(ip), port);
+            socket.Start();
+            Log.Debug($"Socket started on {ip}:{port}");
+            ConnProc = new SockSConnectionsProcessor(socket);
+            ConnProc.StartAccepting();
+            Log.Debug("Started accepting connections");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Debug("All patches applied");
+            sw.Stop();
+            Log.Notice($"Preparation of SocketSaber took {sw.Elapsed}");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit() {
+            ConnProc.Dispose();
+        }
+
+    }
+}
